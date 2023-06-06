66848eae19d332f27a013f2cf06576f5565a9bfe
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-06-06 13:26:23 +0000

    Continue working on planar kinematic control

--- hsa_kinematic_control/hsa_kinematic_control/planar_kinematic_control.py
+++ hsa_kinematic_control/hsa_kinematic_control/planar_kinematic_control.py
@@ -2,14 +2,12 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 
+from dynamixel_sdk_custom_interfaces.msg import SetPosition
 from dynamixel_sdk_custom_interfaces.srv import GetPosition
 
 class PlanarKinematicControlNode(Node):
     def __init__(self):
         super().__init__("planar_kinematic_control_node")
-
-        self.node_frequency = 100 # Hz
-        self.timer = self.create_timer(1.0/self.node_frequency, self.timer_callback)
 
         self.motor_pos_cli = self.create_client(GetPosition, "/get_position")
         self.motor_pos_cli.wait_for_service()
@@ -17,11 +15,32 @@ class PlanarKinematicControlNode(Node):
         self.motor_ids = np.array([21, 22, 23, 24])
         self.motor_neutral_position = self.get_motor_positions()
 
-    def timer_callback(self):
-        self.get_logger().info("timer_callback")
+        self.motor_target_pos_pub = self.create_publisher(SetPosition, "/set_position", 10)
+
+        self.dummy_time_idx = 0
+
+        # PID control
+        self.kp = np.array([1, 0, 1])  # control bending and length, but not shear
+        self.ki = np.array([0, 0, 0])
+        self.kd = np.array([0, 0, 0])
+        self.integral = np.zeros(3)
+
+        self.node_frequency = 100 # Hz
+        self.timer = self.create_timer(1.0/self.node_frequency, self.timer_callback)
+
+    def timer_callback(self, event=None):
+        # print("timer_callback", type(event), event)
+        self.dummy_time_idx += 1
+        if self.dummy_time_idx == 500:
+            print("dummy_time_idx", self.dummy_time_idx)
+            new_motor_positions = self.motor_neutral_position.copy()
+            new_motor_positions[0] += 20
+            print("New motor positions", new_motor_positions)
+            print("Neutral motor positions", self.motor_neutral_position)
+            self.set_motor_goal_positions(self.motor_neutral_position)
 
     def get_motor_positions(self) -> np.ndarray:
-        motor_positions = np.zeros(len(self.motor_ids))
+        motor_positions = np.zeros(len(self.motor_ids), dtype=np.uint32)
         for motor_idx, motor_id in enumerate(list(self.motor_ids)):
             req = GetPosition.Request()
             req.id = int(motor_id)
@@ -33,6 +52,27 @@ class PlanarKinematicControlNode(Node):
             motor_positions[motor_idx] = resp.position
             
         return motor_positions
+    
+    def set_motor_goal_positions(self, goal_positions: np.ndarray):
+        for motor_idx, motor_id in enumerate(list(self.motor_ids)):
+            msg = SetPosition()
+            msg.id = int(motor_id)
+            msg.position = int(goal_positions[motor_idx].item())
+
+            self.motor_target_pos_pub.publish(msg)
+            
+        return
+    
+    def evaluate_pid(self, q: np.ndarray, q_d: np.ndarray = None) -> np.ndarray:
+        if q_d is None:
+            q_d = np.zeros_like(q)
+
+        e = q_d - q
+        u = self.kp * e + self.ki * self.integral + self.kd * (e - self.prev_error)
+
+        self.integral += e
+        self.prev_error = e
+        return u
 
 
 def main(args=None):
