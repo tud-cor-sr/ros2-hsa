b1f1c0a63514b8b72928f29229dc14ce8aa7b2b5
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-06-06 13:53:37 +0000

    Add recording of rosbag to launch file

--- launch/hsa_planar_kinematic_cl_launch.py
+++ launch/hsa_planar_kinematic_cl_launch.py
@@ -1,9 +1,12 @@
 # CLosed-loop kinematic controller for planar HSA robot
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import ExecuteProcess, Node
+
+RECORD_BAG = False   # Record data to rosbag file
+BAG_PATH = "/home/mstoelzle/phd/rosbags"
 
 def generate_launch_description():
-    return LaunchDescription([
+    launch_actions = [
         Node(
             package='hsa_inverse_kinematics',
             executable='planar_cs_ik_node',
@@ -19,4 +22,12 @@ def generate_launch_description():
             executable='planar_kinematic_control_node',
             name='kinematic_control'
         )
-    ])
+    ]
+
+    if RECORD_BAG:
+        launch_actions.append(ExecuteProcess(
+            cmd=['ros2', 'bag', 'record', '-a', '-o', BAG_PATH],
+            output='screen'
+        ))
+
+    return LaunchDescription(launch_actions)
