90614fd7b91f801e4c42bf68f32f31fb9682a618
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-07-05 21:33:16 +0000

    Some fixes to the inverse kineamtics launch file

--- launch/hsa_inverse_kinematics_launch.py
+++ launch/hsa_inverse_kinematics_launch.py
@@ -9,34 +9,35 @@ RECORD_BAG = False  # Record data to rosbag file
 BAG_PATH = "/home/mstoelzle/phd/rosbags"
 LOG_LEVEL = "warn"
 
+
 def generate_launch_description():
     # Create the NatNet client node
     natnet_config = os.path.join(
-        get_package_share_directory('mocap_optitrack_client'),
-        'config',
-        'natnetclient.yaml'
+        get_package_share_directory("mocap_optitrack_client"),
+        "config",
+        "natnetclient.yaml",
     )
     # Create the world to base client
     w2b_config = os.path.join(
-        get_package_share_directory('hsa_inverse_kinematics'),
-        'config',
-        'world_to_base_y_up.yaml'
+        get_package_share_directory("hsa_inverse_kinematics"),
+        "config",
+        "world_to_base_y_up.yaml",
     )
 
     launch_actions = [
         Node(
-            package='mocap_optitrack_client',
-            executable='mocap_optitrack_client',
-            name='natnet_client',
-            parameters = [natnet_config],
-            arguments=['--ros-args', '--log-level', LOG_LEVEL]
+            package="mocap_optitrack_client",
+            executable="mocap_optitrack_client",
+            name="natnet_client",
+            parameters=[natnet_config],
+            arguments=["--ros-args", "--log-level", LOG_LEVEL],
         ),
         Node(
-            package='mocap_optitrack_w2b',
-            executable='mocap_optitrack_w2b',
-            name='world_to_base',
-            parameters = [w2b_config],
-            arguments=['--ros-args', '--log-level', LOG_LEVEL]
+            package="mocap_optitrack_w2b",
+            executable="mocap_optitrack_w2b",
+            name="world_to_base",
+            parameters=[w2b_config],
+            arguments=["--ros-args", "--log-level", LOG_LEVEL],
         ),
         Node(
             package="hsa_inverse_kinematics",
